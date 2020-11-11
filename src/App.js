5386b9cd30d7172f0e5e453617310aed1ab8fe48
Tautvydas Riekašius <tautvydas@baltictours.lt>
2020-11-11 15:03:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 1280px;
+  max-width: 100%;
+  margin: 0 auto;
+  th {
+    text-align: left;
+    padding: 10px;
+    background: #f5f5f5;
+  }
+  td {
+    border-bottom: 1px solid #f5f5f5;
+    padding: 5px;
+  }
+`;
+const TopHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  input {
+    width: 400px;
+    padding: 10px;
+  }
+`;
 
 function App() {
+  const [employees, updateEmployees] = useState([]);
+
+  if (employees == 0) {
+    document.title = "Loading...";
+  }
+
+  useEffect(() => {
+    fetch("http://dummy.restapiexample.com/api/v1/employees")
+      .then(res => res.json())
+      .then(result => {
+        updateEmployees(result.data);
+        document.title = `Total: ${result.data.length} `;
+      });
+  }, []);
+
+  const [searchValue, updateSearch] = useState("");
+
+  const handleSearch = e => {
+    updateSearch(e.target.value);
+
+    const filteredEmpl = employees.filter(empl =>
+      empl.employee_name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    console.log(filteredEmpl);
+    updateEmployees(filteredEmpl);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TopHeader>
+        <div>
+          Total employees: <strong>{employees.length}</strong>
+          <strong>{searchValue}</strong>
+        </div>
+        <div>
+          <input
+            type="text"
+            onChange={handleSearch}
+            value={searchValue}
+            placeholder="search"
+          />
+        </div>
+      </TopHeader>
+
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Employee name</th>
+            <th>Employee salary</th>
+            <th>Employee age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map(employee => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.employee_name}</td>
+              <td>{employee.employee_salary}</td>
+              <td>{employee.employee_age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
   );
 }
 
